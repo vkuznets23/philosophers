e3ab7975e99cada985ac418d18267a580ba4dd75
Виктория Кузнецова <victoria.cuzneczowa23@gmail.com>
2024-09-08 20:12:15 +0300

    fixed norm errors in the philo.h file

--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: viktoria <viktoria@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/08 18:09:54 by viktoria          #+#    #+#             */
-/*   Updated: 2024/09/08 18:23:14 by viktoria         ###   ########.fr       */
+/*   Updated: 2024/09/08 20:11:38 by viktoria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,28 +33,28 @@ typedef struct s_philo	t_philo;
 
 typedef struct s_table
 {
-	t_philo		*philos;
-	size_t		nb_philos;
-	size_t		time_to_die;
-	size_t		time_to_eat;
-	size_t		time_to_sleep;
-	size_t		must_eat_count;
-	size_t		no_full;
-	size_t		start;
-	int			dead_or_full;
 	pthread_mutex_t		*forks;
 	pthread_mutex_t		locks;
+	t_philo				*philos;
+	size_t				nb_philos;
+	size_t				time_to_die;
+	size_t				time_to_eat;
+	size_t				time_to_sleep;
+	size_t				must_eat_count;
+	size_t				no_full;
+	size_t				start;
+	int					dead_or_full;
 }	t_table;
 
 typedef struct s_philo
 {
-	pthread_t			thread;
-	t_table				*table;
 	pthread_mutex_t		*fork_l;
 	pthread_mutex_t		*fork_r;
-	size_t		id;
-	size_t		lst_eating;
-	size_t		no_ate;
+	pthread_t			thread;
+	t_table				*table;
+	size_t				id;
+	size_t				lst_eating;
+	size_t				no_ate;
 }	t_philo;
 
 /******************************************************************************
@@ -62,31 +62,31 @@ typedef struct s_philo
 ******************************************************************************/
 
 //parsing
-int	is_valid(int ac, char **av);
+int		is_valid(int ac, char **av);
 //int	ft_atoi(char *str);
-int	ft_ctos(const char *str, size_t *result);
+int		ft_ctos(const char *str, size_t *result);
 
 //init.c
-int	init_table(int ac, char **av, t_table *table);
+int		init_table(int ac, char **av, t_table *table);
 
 //start_threads
-int	ft_join_threads(t_table *table);
-int	ft_start_threads(t_table *table);
-int	join_thread(t_table *table, int i);
-int	create_thread(t_table *table, int i);
+int		ft_join_threads(t_table *table);
+int		ft_start_threads(t_table *table);
+int		join_thread(t_table *table, int i);
+int		create_thread(t_table *table, int i);
 
 //philo_routine
-void		*philo_routine(void *arg);
-void		eating_time(t_philo *philo);
 size_t	get_time(void);
-int	ft_wait(size_t ms, t_table *table);
-int	time_to_stop_sim(t_philo *philo);
-int	ft_prnt_lock(t_philo *philo, const char *activity);
-int	grab_forks(t_philo *philo);
+void	*philo_routine(void *arg);
+void	eating_time(t_philo *philo);
+int		ft_wait(size_t ms, t_table *table);
+int		time_to_stop_sim(t_philo *philo);
+int		ft_prnt_lock(t_philo *philo, const char *activity);
+int		grab_forks(t_philo *philo);
 
 //terminate
-void		ft_terminate(t_table *table);
-int	ft_usage(void);
+void	ft_terminate(t_table *table);
+int		ft_usage(void);
 
 //stop_threads
 void	ft_stop_simulation(t_table *table);
